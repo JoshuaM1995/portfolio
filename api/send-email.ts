2048a181e57aba2af29cd64e92530d3a6de1968e
Joshua McNabb <joshuamcnabb1995@gmail.com>
2023-09-25 19:16:11 -0600

    add email functionality

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,13 +1,42 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import "dotenv/config";
+import postmark from "postmark";
+import dotenv from "dotenv";
 
-export default function handler(
+dotenv.config({ path: "./api/.env" });
+
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
+  if (!request.url) return response.status(400);
+
+  const { searchParams } = new URL(
+    request.url,
+    `http://${request.headers.host}`
+  );
+
+  const name = searchParams.get("name");
+  const email = searchParams.get("email");
+  const subject = searchParams.get("subject");
+  const message = searchParams.get("message");
+
+  const client = new postmark.ServerClient(process.env.POSTMARK_API_TOKEN);
+
+  const res = await client.sendEmail({
+    From: "info@joshuamcnabb.ca",
+    To: process.env.BUSINESS_EMAIL,
+    Subject: `Contact Form Submission - ${subject}`,
+    HtmlBody: `<b>Name:</b> ${name}<br />
+      <b>Email:</b> ${email}<br />
+      <b>Message:</b><br />
+      ${message}<br />`,
+    TextBody: "Testing",
+    MessageStream: "outbound",
   });
+
+  if (res.Message === "OK") {
+    return response.status(200).json({ success: true });
+  }
+
+  return response.status(400).json({ success: false });
 }
