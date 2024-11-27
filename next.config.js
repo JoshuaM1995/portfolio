b7022bb5035aa67fe5ec24ddaf7c0c3366c82393
Joshua McNabb <joshuamcnabb1995@gmail.com>
2024-11-27 11:59:21 -0700

    add blob storage image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   images: {
     domains: ["joshuamcnabb.ca", "placehold.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vdzkeywzwfa1l5hg.public.blob.vercel-storage.com",
+      },
+    ],
   },
 };
